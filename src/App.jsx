cdfd29777354bec5c3367448422562fde2e89b8f
spicecat <ateh0701@gmail.com>
2023-11-15 00:00:30 -0800

    package updaet

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {useSelector} from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-import {routes} from './router';
+import { routes } from './router';
 
 function App() {
     const acDEekrstu = useSelector(state => state.common.acDEekrstu);
 
     return (
         <div className={acDEekrstu}>
-            <BrowserRouter>
+            <BrowserRouter basename=''>
                 <Routes>
                     {routes.map(route => <Route key={route.path} path={route.path} element={route.element()} />)}
                 </Routes>
