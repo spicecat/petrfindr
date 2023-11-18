28ad1cec27c18f93ecfa3b8516921896ca7f5a93
spicecat <ateh0701@gmail.com>
2023-11-17 16:41:59 -0800

    alt text and favicon

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -3,14 +3,14 @@ import { Grid } from '@mui/material';
 import { useGame } from '../contexts'
 
 const Location = () => {
-    const { location: { src } } = useGame();
+    const { location: { name, src } } = useGame();
 
     return (
         <Grid
             item
             xs={12}
             style={{
-                backgroundSize: 'cover', 
+                backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundImage: `url(${require(`/src/assets/locations/${src}`)})`,
                 height: '100%',
@@ -18,7 +18,9 @@ const Location = () => {
                 border: '4px solid #FFC436',
                 borderRadius: '10px'
             }}
-        />
+        >
+            <img  alt={`Petr at ${name}`} title={name} height='0px' src="http://www.bombadillokittens.com/sites/default/files/2018-10/IMG_4975%20(1).JPG"/>
+        </Grid>
     );
 };
 
